bb3d1178815099e8f08344d816bac65c2d16af9a
eyhdev <alaqsa335@gmail.com>
2023-08-15 18:25:13 +0300

    Initial Commit

---
+++ History_App/History_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  History_AppApp.swift
+//  History_App
+//
+//  Created by eyh.mac on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct History_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
